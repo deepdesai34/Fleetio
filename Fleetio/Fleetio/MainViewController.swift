41fd27cc43b1ae20463cf0fda2356a6ade3bcd2c
deepdesai34 <85068684+deepdesai34@users.noreply.github.com>
2022-11-13 17:25:16 -0500

    setup app without storyboards, in order to build app with programatic ui

---
+++ Fleetio/Fleetio/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fleetio
+//
+//  Created by Deep Desai on 2022-11-13.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
