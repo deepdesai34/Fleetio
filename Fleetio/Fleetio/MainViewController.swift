13356ac3fe7747b0310f425ccf06443c811f0e6a
deepdesai34 <85068684+deepdesai34@users.noreply.github.com>
2022-11-14 21:30:21 -0500

    began binding data to mainVC

--- Fleetio/Fleetio/MainViewController.swift
+++ Fleetio/Fleetio/MainViewController.swift
@@ -26,7 +26,7 @@ class MainViewController: UIViewController {
         
         return table
     }()
-
+    
     override func viewDidLoad() {
         view.backgroundColor = .white
         super.viewDidLoad()
@@ -35,10 +35,10 @@ class MainViewController: UIViewController {
         
     }
     
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        tableView.frame = view.bounds
-//    }
+    //    override func viewDidLayoutSubviews() {
+    //        super.viewDidLayoutSubviews()
+    //        tableView.frame = view.bounds
+    //    }
     
     func configureViews() {
         view.addSubview(tableView)
@@ -58,6 +58,9 @@ class MainViewController: UIViewController {
         // Binding data for vehicles from viewModel
         mainVM.getVehicles { newVehicles in
             self.vehicles = newVehicles
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
     }
 }
@@ -66,7 +69,7 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource  {
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let view = UIView()
-        view.layer.cornerRadius = 10
+        view.layer.cornerRadius = 5
         
         view.backgroundColor = .fleetioGreen
         
@@ -74,36 +77,56 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource  {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont(name: "Courier-BoldOblique", size: 25)
         label.textColor = .white
-        label.text = "heyyyy"
+        
+        
+        label.text = vehicles[section].name
         view.addSubview(label)
         
         NSLayoutConstraint.activate([
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            ])
+        ])
         
         return view
     }
     
-    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 30
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 5
+        return vehicles.count
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 4
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         var configuration = cell.defaultContentConfiguration()
-        configuration.text = "hello"
+        
+        switch indexPath.row {
+        case 0:
+            configuration.text = vehicles[indexPath.section].make
+            
+        case 1:
+            configuration.text = vehicles[indexPath.section].make
+            
+        case 2:
+            configuration.text = vehicles[indexPath.section].make
+            
+        case 3:
+            configuration.text = vehicles[indexPath.section].make
+            
+        default:
+            configuration.text = ""
+            
+        }
+        
         cell.contentConfiguration = configuration
         
-
+        
         return cell
     }
 }
